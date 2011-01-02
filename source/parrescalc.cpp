fa9fbd6eddce536bead5be9d878438940c5c37c2
eresonance <eresonance@users.noreply.github.com>
2011-01-02 16:46:14 +0000

    For some reason the first part of the main processing function got lopped off before the last commit. Restored to original working order.

--- source/parrescalc.cpp
+++ source/parrescalc.cpp
@@ -1,10 +1,3 @@
-/*  Copyright (c) 2010, Devin Linnington
-    All rights reserved.
-    eresonance@gmail.com
-
-    See LICENSE.txt for rights of use
-*/
-
 #include "parrescalc.h"
 #include "ui_parrescalc.h"
 //#include <QtDebug>
@@ -13,7 +6,6 @@
 #include <QDesktopServices>
 #include <QUrl>
 
-//standard resistor values
 const double one[] =  { 10.0, 10.2, 10.5, 10.7, 11.0,
                         11.3, 11.5, 11.8, 12.1, 12.4,
                         12.7, 13.0, 13.3, 13.7, 14.0,
@@ -55,6 +47,82 @@ ParResCalc::~ParResCalc()
 }
 
 void ParResCalc::on_btnCalc_clicked()
+{
+    //Main processing
+    double neededR = ui->spnResNeeded->value();
+    int decade = 0;
+    while(neededR > 50.0)
+    {
+        neededR /= 10.0;
+        decade++;
+    }
+    if(neededR < 10.0)
+    {
+        neededR *= 10.0;
+        decade--;
+    }
+    double tol = 0.0;
+    switch(ui->cmbPercent->currentIndex())
+    {
+    case 0://1%
+        tol = 0.01;
+
+        break;
+    case 1://5%
+        tol = 0.05;
+        break;
+    }
+    QVarLengthArray<double> r;
+    if(tol == 0.01)
+        r.append(one,96);
+    else if(tol == 0.05)
+        r.append(five,24);
+    else
+    {
+        //qDebug() << "ERROR: invalid tol in r assign" << endl;
+        return;
+    }
+
+    double lowlim = (1-tol)*neededR;
+    double hilim = (1+tol)*neededR;
+
+    double best[] = {0,0,0};
+    double cur = 0.0;
+
+    //iterate through each decade
+    for(int dec=1;dec < 5;dec++)
+    {
+        //go through the list of resistors
+        for(int i=0;i<r.size();i++)
+        {
+            for(int j=i;j<r.size();j++)
+            {
+                //par res calculation
+                cur = (r[i] * r[j]) / (r[i] + r[j]);
+                if((cur>=lowlim)&&(cur<=hilim))
+                {
+                    if(qAbs(neededR - cur) < qAbs(neededR - best[0]))
+                    {
+                        // new value better
+                        best[0] = cur;
+                        best[1] = r[i];
+                        best[2] = r[j];
+                    }
+                }
+            }
+        }
+        if(best[0] != 0.0) //found a suitable pair!
+            break;
+        //double size of r with new decade
+        //qDebug() << "Doubling r, adding next decade " << dec;
+        r.reserve(r.size()*2);
+        int rsize = r.size();
+        double temp = pow(10,dec);
+        for(int i=0;i<rsize;i++)
+        {
+            r.append(r[i] * temp);
+        }
+    }
     //convert the resistances back to the right decade
     best[0] *= pow(10,decade);
     best[1] *= pow(10,decade);
@@ -70,6 +138,7 @@ void ParResCalc::on_btnCalc_clicked()
     const char *r1suf = &suffix[ui->cmbSuffix->currentIndex()];
     const char *r2suf = &suffix[ui->cmbSuffix->currentIndex()];
     const char *actsuf = &suffix[ui->cmbSuffix->currentIndex()];
+
     if(best[0]<1000.00)
     {
         strActual.append(QString("%1%2").arg(best[0],0,'f',3).arg(*actsuf));
